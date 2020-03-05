c37cb1906ce6bb3288c6f1e58da0f86ce5da0c44
Daniel Cregg <danielcregg@users.noreply.github.com>
2020-03-05 13:40:11 +0000

    Initial commit

---
+++ Bicycle.java
@@ -0,0 +1,26 @@
+public class Bicycle {
+
+	// the Bicycle class has three fields
+	public int cadence;
+	public int gear;
+	public int speed;
+	
+	// Default constructor
+
+	// Parameterised constructor
+//	public Bicycle(int startCadence, int startSpeed, int startGear) {
+//		// Your code here
+//	}
+	
+	// Getters and Setters here
+
+	// Extra methods here
+	public void applyBrake(int speedDecrement) {
+		speed -= speedDecrement;
+	}
+
+	public void speedUp(int speedIncrement) {
+		// Your code here
+	}
+
+}
---
+++ MountainBike.java
@@ -0,0 +1,18 @@
+public class MountainBike extends Bicycle {
+    
+    // the MountainBike subclass adds one field
+    public int seatHeight;
+
+    // Default Constructor
+    public MountainBike() {
+    	
+    }
+    // Paramiterised constructor
+//    public MountainBike(int startHeight) {
+//    	seatHeight = startHeight;
+//    }  
+    
+    // Paramiterised constructor using super keyword 
+        
+    // Getters and Setters
+}
